4eb31cb9d60feb1b7aaeed833a4f6c19e87529b5
jeison Garcia <jeison.95garcia@gmail.com>
2025-06-27 12:27:32 -0500

    Cambio de lógica de cierre de sesión

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,22 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
-header('location:index.php');
 
+// Limpieza de todas las variables de sesión
+$_SESSION = array();
+
+// Eliminación de la cookie de sesión si se utiliza:
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destrucción de la sesión
+session_destroy();
+
+// Redirección al inicio
+header("Location: index.php");
+exit();
 ?>
